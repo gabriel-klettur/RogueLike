1b17678f5e46b87f72e9e24a525fa1d4fed555a7
Gabriel Roca <blast.blackstone@gmail.com>
2025-04-10 18:23:57 +0000

    chore(player): changed name from 'base' to 'player'
    better description name and 'base' its generic

--- roguelike_project/engine/game/systems/entity_loader.py
+++ roguelike_project/engine/game/systems/entity_loader.py
@@ -1,4 +1,4 @@
-from roguelike_project.entities.player.base import Player
+from roguelike_project.entities.player.player import Player
 from roguelike_project.loaders.load_obstacles import load_obstacles
 from roguelike_project.loaders.load_enemies import load_enemies
 from roguelike_project.loaders.load_buildings import load_buildings
