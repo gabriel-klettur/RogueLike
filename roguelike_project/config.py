359d4ff44d388da3cba07e870c6536f372876e08
Gabriel Roca <blast.blackstone@gmail.com>
2025-04-18 16:20:09 +0000

    Update config.py
    Cambios en las dimensiones del lobby

--- roguelike_project/config.py
+++ roguelike_project/config.py
@@ -34,8 +34,8 @@ DUNGEON_WIDTH = 120
 DUNGEON_HEIGHT = 120
 
 # Tamaño lógico del lobby insertado
-LOBBY_WIDTH = 80   # (basado en la cantidad de caracteres por línea)
-LOBBY_HEIGHT = 32  # (número de líneas)
+LOBBY_WIDTH = 100   # (basado en la cantidad de caracteres por línea)
+LOBBY_HEIGHT = 100  # (número de líneas)
 
 # Offset visual recomendado
 LOBBY_OFFSET_X = 5
