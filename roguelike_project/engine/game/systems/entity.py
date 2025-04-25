c897d34a14f3e4539e677df8844a778f02906714
Gabriel Roca <blast.blackstone@gmail.com>
2025-04-25 13:22:06 +0000

    refactor(loaders): Agregados al 'engine/game' para futuras refactorizaciones.

--- roguelike_project/engine/game/systems/entity.py
+++ roguelike_project/engine/game/systems/entity.py
@@ -1,7 +1,7 @@
 from roguelike_project.engine.game.entities.player.player import Player
-from roguelike_project.loaders.load_obstacles import load_obstacles
-from roguelike_project.loaders.load_enemies import load_enemies
-from roguelike_project.loaders.load_buildings import load_buildings
+from roguelike_project.engine.game.loaders.load_obstacles import load_obstacles
+from roguelike_project.engine.game.loaders.load_enemies import load_enemies
+from roguelike_project.engine.game.loaders.load_buildings import load_buildings
 
 def load_entities(z_state=None):
     player = Player(600, 600)
