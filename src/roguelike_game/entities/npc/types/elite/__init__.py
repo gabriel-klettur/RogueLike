86e578512e74bd5d07a603104d81935f80db6a82
Gabriel Roca <blast.blackstone@gmail.com>
2025-05-03 18:41:52 +0000

    refactor(npc): creacion paquete 'npc/utils' y creacion esqueleto 'npc/types'

---
+++ src/roguelike_game/entities/npc/types/elite/__init__.py
@@ -0,0 +1,5 @@
+#Path: src/roguelike_game/entities/npc/types/elite/__init__.py
+
+"""
+Configuración y assets para el tipo 'elite'.
+"""
---
+++ src/roguelike_game/entities/npc/types/monster/__init__.py
@@ -0,0 +1,6 @@
+# src/roguelike_game/entities/npc/types/monster/__init__.py
+
+"""
+Configuración y assets para el tipo 'monster'.
+El factory leerá el config.yaml y registrará este tipo.
+"""
