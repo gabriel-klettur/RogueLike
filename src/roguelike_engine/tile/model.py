dcbf1f5bec1dc1a17caf366a4eb8296f65f3a397
Gabriel Roca <blast.blackstone@gmail.com>
2025-05-18 21:22:34 +0000

    refactor(tile): Eliminada funcion obsoleta.

--- src/roguelike_engine/tile/model.py
+++ src/roguelike_engine/tile/model.py
@@ -23,20 +23,3 @@ class Tile:
         self.solid = (tile_type == "#")
         self.rect = pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)
         self.scaled_cache: dict[float, pygame.Surface] = {}
-
-    def render_tiles(self, screen: pygame.Surface, camera) -> pygame.Rect | None:
-        """
-        Dibuja el sprite escalado según la cámara y retorna el rect de colisión.
-        """
-        if not camera.is_in_view(self.x, self.y, self.sprite_size):
-            return None
-
-        zoom = round(camera.zoom * 10) / 10.0
-        if zoom not in self.scaled_cache:
-            scaled_size = camera.scale(self.sprite_size)
-            self.scaled_cache[zoom] = pygame.transform.scale(self.sprite, scaled_size)
-
-        scaled_sprite = self.scaled_cache[zoom]
-        screen.blit(scaled_sprite, camera.apply((self.x, self.y)))
-
-        return self.rect
