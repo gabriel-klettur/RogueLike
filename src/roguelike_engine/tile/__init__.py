fdb7ee9d78e45aa6a2b447f0e1e0e19927a606e2
Gabriel Roca <blast.blackstone@gmail.com>
2025-05-18 21:28:57 +0000

    refactor(tile): Eliminadas funciones obsoletas cuya responsabilidad actual es recogida en nuestro 'map'

--- src/roguelike_engine/tile/__init__.py
+++ src/roguelike_engine/tile/__init__.py
@@ -3,13 +3,10 @@
 from .model import Tile
 from .assets import get_sprite_for_tile, load_base_tile_images
 from .loader import load_tiles_from_text
-from .overlay import load_overlay_map, save_overlay_map
 
 __all__ = [
     "Tile",
     "get_sprite_for_tile",
     "load_base_tile_images",
-    "load_tiles_from_text",
-    "load_overlay_map",
-    "save_overlay_map",
+    "load_tiles_from_text"    
 ]
